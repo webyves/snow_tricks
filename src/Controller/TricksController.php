9fdcb56e73b39832a6622f59a2011a9d13b94b03
yBernier <webyves@hotmail.com>
2019-01-12 15:31:03 +0100

    sparate Add & Edit Tricks

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -24,17 +24,12 @@ class TricksController extends AbstractController
     public function showTrick(Tricks $trick, Request $request, ObjectManager $manager)
     {
         $trickComment = new TrickComment();
-
-        // $trickComment = TrickComment::create();
-        
         $form = $this->createForm(TrickCommentType::class, $trickComment);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $user = $this->getUser();
-
             $trickComment->setDateCreate(new \DateTime())
                          ->setTrick($trick)
-                         ->setUserCreate($user)
+                         ->setUserCreate($this->getUser())
                          ->setText(strip_tags($trickComment->getText()));
 
             $manager->persist($trickComment);
@@ -53,39 +48,50 @@ class TricksController extends AbstractController
 
     /**
      * @Route("/trick/add", name="add_trick")
-     * @Route("/trick/edit/{id}", name="edit_trick")
      */
-    public function formTricks(Tricks $trick = null, Request $request, ObjectManager $manager)
+    public function addFormTricks(Request $request, ObjectManager $manager)
     {
-        if (!$trick) {
-            $trick = new Tricks();
-        }
-
+        $trick = new Tricks();
         $formTrick = $this->createForm(TrickType::class, $trick);
-
         $formTrick->handleRequest($request);
         if ($formTrick->isSubmitted() && $formTrick->isValid()) {
-            $user = $this->getUser();
-            $msg = "modifiée";
-            if(!$trick->getId()){
-                $msg = "ajoutée";
-                $trick->setDateCreate(new \DateTime())
-                      ->setUserCreate($user);
-            }
-            $trick->setDateUpdate(new \DateTime())
-                  ->setUserUpdate($user);
+            $trick->setDateCreate(new \DateTime())
+                  ->setUserCreate($this->getUser())
+                  ->setDateUpdate(new \DateTime())
+                  ->setUserUpdate($this->getUser());
 
             $manager->persist($trick);
             $manager->flush();
-            $this->addFlash('success', 'Votre Figure à bien été '.$msg.',<br><strong>Merci</strong>.');
+            $this->addFlash('success', 'Votre Figure à bien été ajoutée,<br><strong>Merci</strong>.');
             return $this->redirectToRoute('show_trick', ['id' => $trick->getId()]);
         }
-                    
 
         return $this->render('snow/formTrick.twig', [
             'formTrick' => $formTrick->createView(),
-            'trick' => $trick,
-            'editMode' => $trick->getId() !== null
+            'trick' => $trick
+        ]);
+    }
+
+    /**
+     * @Route("/trick/edit/{id}", name="edit_trick")
+     */
+    public function editFormTricks(Tricks $trick, Request $request, ObjectManager $manager)
+    {
+        $formTrick = $this->createForm(TrickType::class, $trick);
+        $formTrick->handleRequest($request);
+        if ($formTrick->isSubmitted() && $formTrick->isValid()) {
+            $trick->setDateUpdate(new \DateTime())
+                  ->setUserUpdate($this->getUser());
+
+            $manager->persist($trick);
+            $manager->flush();
+            $this->addFlash('success', 'Votre Figure à bien été modifiée,<br><strong>Merci</strong>.');
+            return $this->redirectToRoute('show_trick', ['id' => $trick->getId()]);
+        }
+
+        return $this->render('snow/formTrick.twig', [
+            'formTrick' => $formTrick->createView(),
+            'trick' => $trick
         ]);
     }
 
